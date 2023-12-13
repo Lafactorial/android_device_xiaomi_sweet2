c4c2b1b13bb9550c404c6477d5ce42d18e17199f
Alexander Winkowski <dereference23@outlook.com>
2023-12-13 14:04:59 +0000

    sweet2: Redmi Note 10 Pro -> Redmi Note 12 Pro
    Change-Id: I1ea3fd8559ff9388abdf250526a6f786c1ed2d8f

--- Android.mk
+++ Android.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021, 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),sweet)
+ifeq ($(TARGET_DEVICE),sweet2)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
