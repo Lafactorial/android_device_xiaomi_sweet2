ec951e93d626cacd95cd38ae967c725f64c7bba4
Alexander Winkowski <dereference23@outlook.com>
2023-12-13 14:04:28 +0000

    sweet2: Update blobs from OS1.0.8.0.THGMIXM
    Change-Id: I2d376b6a5cb322827dee3b4fba1807b9779613aa

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,7 +38,7 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 TARGET_SCREEN_DENSITY := 440
 
 # Vendor security patch level
-VENDOR_SECURITY_PATCH := 2023-11-01
+VENDOR_SECURITY_PATCH := 2024-08-01
 
 # Inherit from proprietary files
 include vendor/xiaomi/sweet2/BoardConfigVendor.mk
