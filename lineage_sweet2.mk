900e2d573a47fcfb1e25b2f0909aafa0e4ad2ba6
Alexander Winkowski <dereference23@outlook.com>
2023-12-13 14:04:59 +0000

    sweet2: Redmi Note 10 Pro -> Redmi Note 12 Pro
    Change-Id: I1ea3fd8559ff9388abdf250526a6f786c1ed2d8f

---
+++ lineage_sweet2.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2021-2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := Redmi
+PRODUCT_DEVICE := sweet2
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 2209116AG
+PRODUCT_NAME := lineage_sweet2
+PRODUCT_SYSTEM_DEVICE := sweet
+PRODUCT_SYSTEM_NAME := sweet_global2
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="sweet_global2-user 13 TKQ1.221114.001 V816.0.13.0.THGMIXM release-keys" \
+    BuildFingerprint=Redmi/sweet_global2/sweet:13/TKQ1.221114.001/V816.0.13.0.THGMIXM:user/release-keys \
+    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
