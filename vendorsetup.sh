79113a7d0520d81c55000d549f2a3b7f27a9aae9
Lafactorial <hknhzl@gmail.com>
2024-10-01 16:56:54 +0300

    sweet2: Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+rm -rf prebuilts/clang/host/linux-x86/clang-r530567
+
+git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r530567.git -b 14.0 prebuilts/clang/host/linux-x86/clang-r530567
