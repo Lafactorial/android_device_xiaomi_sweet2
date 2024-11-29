883e15883ba202ceb672ecdd6927a54fcc9277e8
Lafactorial <hknhzl@gmail.com>
2024-11-29 10:50:40 +0300

    Initialize CipherOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet2.mk
+    $(LOCAL_DIR)/cipher_sweet2.mk
+
+COMMON_LUNCH_CHOICES := \
+    cipher_sweet2-user \
+    cipher_sweet2-userdebug \
+    cipher_sweet2-eng
--- cipher_sweet2.mk
+++ cipher_sweet2.mk
@@ -12,17 +12,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cipher/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := sweet2
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2209116AG
-PRODUCT_NAME := lineage_sweet2
+PRODUCT_NAME := cipher_sweet2
 PRODUCT_SYSTEM_DEVICE := sweet
 PRODUCT_SYSTEM_NAME := sweet_global2
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Cipher Ify
+CIPHER_MAINTAINER := HakaN
+CIPHER_OFFICIAL := false
+CIPHER_GAPPS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sweet_global2-user 13 TKQ1.221114.001 V816.0.6.0.THGMIXM release-keys" \
