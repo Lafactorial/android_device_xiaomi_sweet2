eede88629d11950b4ab22df9aeba1ca7932b85e0
lemezohaib <zhbkhan644@gmail.com>
2025-01-18 13:46:59 +0100

    sweet2: Welcome Pixelage
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet2.mk
+    $(LOCAL_DIR)/pixelage_sweet2.mk
--- pixelage_sweet2.mk
+++ pixelage_sweet2.mk
@@ -12,14 +12,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Pixelage stuff.
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+# Pixelage
+PIXELAGE_BUILDTYPE := UNOFFICIAL
+PIXELAGE_MAINTAINER := HaKaN
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := sweet2
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2209116AG
-PRODUCT_NAME := lineage_sweet2
+PRODUCT_NAME := pixelage_sweet2
 PRODUCT_SYSTEM_DEVICE := sweet
 PRODUCT_SYSTEM_NAME := sweet_global2
 
