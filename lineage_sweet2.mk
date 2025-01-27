0279272734d5f813e62e678fb9c032f7443bee84
Lafactorial <hknhzl@gmail.com>
2025-01-27 16:06:04 +0300

    sweet2: RisingOS init gapps build

--- lineage_sweet2.mk
+++ lineage_sweet2.mk
@@ -29,4 +29,21 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sweet_global2-user 13 TKQ1.221114.001 V816.0.13.0.THGMIXM release-keys" \
     BuildFingerprint=Redmi/sweet_global2/sweet:13/TKQ1.221114.001/V816.0.13.0.THGMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
-    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
+    RisingChipset=Snapdragon 732G \
+    RisingMaintainer=HaKaN
+
+# RisingTechOss Stuff
+WITH_GMS := true
+TARGET_CORE_GMS := true
+RISING_STORAGE := 256GB
+RISING_RAM := 8GB
+RISING_BATTERY := 5000mAh
+RISING_DISPLAY := 1080 x 2400
+RISING_BUILDTYPE := UNOFFICIAL
+PRODUCT_NO_CAMERA := false
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
