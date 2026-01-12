ad492161ed2470b6da288b2447e831255409a71e
basamaryan <basam.aryan@gmail.com>
2026-01-12 10:46:16 +0400

    sweet2: Move to vendor/oneplus/dolby
    Change-Id: I419e1b656637503f0fda80da3d73f6eda481cd1d

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,6 @@ echo 'Starting to clone stuffs needed to build for sweet2'
 
 # Dolby Stuff
 echo 'Cloning Dolby Stuff'
-rm -rf device/oneplus/dolby && git clone https://github.com/aosp-playground/android_device_oneplus_dolby.git -b sixteen-qpr1 device/oneplus/dolby
-rm -rf vendor/oneplus/dolbys && git clone https://github.com/aosp-playground/proprietary_vendor_oneplus_dolby.git -b sixteen-qpr1 vendor/oneplus/dolby
+rm -rf vendor/oneplus/dolby && git clone https://gitlab.com/NoPrincessHere/proprietary_vendor_oneplus_dolby.git -b sixteen-qpr1 vendor/oneplus/dolby
 
 echo 'Cloning process is completed, now its time for lunch'
